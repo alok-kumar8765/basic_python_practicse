cc098115541cb88bd0e3511c4ad5cb24ecc1086f
alok8765 <85283226+alok-techqware@users.noreply.github.com>
2021-07-09 20:47:32 +0530

    error resolved

--- working_with_pdf_csv/working_with_pdf.py
+++ working_with_pdf_csv/working_with_pdf.py
@@ -18,7 +18,7 @@ first_page=pdf_reader.getPage(0)
 pdf_writer=PyPDF2.PdfFileWriter()
 print(type(first_page))
 pdf_writer.addPage(first_page)
-pdf_output=open('Some_BrandNew_Doc.pdf','rb')
+pdf_output = open("Some_New_Doc.pdf","wb")
 pdf_writer.write(pdf_output)
 f.close()
 pdf_output.close()
@@ -27,7 +27,7 @@ f=open('Working_Business_Proposal.pdf','rb')
 
 pdf_text=[]
 pdf_reader=PyPDF2.PdfFileReader(f)
-for num in range(pdf_reader.numPage):
+for num in range(pdf_reader.numPages):
     page=pdf_reader.getPage(num)
     pdf_text.append(page.extractText())
 print(pdf_text[1])
